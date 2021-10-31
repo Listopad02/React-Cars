e253b819eacc2fa071d38f2f4d82f28469ff74af
Listopad02 <123ayetvbass@gmail.com>
2021-10-31 16:05:29 +0300

    first commit in progect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car';
+import { useState } from 'react';
 
 function App() {
+  const state = {
+    cars: [
+      {name: 'Ford Focus', year: 2016},
+      {name: 'Audi A7', year: 2019},
+      {name: 'Mazda CX5', year: 2017}
+    ]
+  }
+
+  const [ pageTitle, setPageTitle ] = useState('Cars');
+  const changeTitleHandler = newTitle => {
+    setPageTitle(newTitle);
+  }
+
+  const [ showCars, setShowCars ] = useState(false);
+  const toggleCarsHandler = () => {
+    setShowCars(!showCars);
+  }
+
+  const divStyle = {
+    textAlign: 'center',
+  }
+
+  let cars = null;
+
+  if (showCars) {
+    cars = state.cars.map((car, i) => {
+      return (
+        <Car 
+          key={i} name={car.name} year={car.year}
+          onChangeTitle={() => changeTitleHandler(car.name)}
+        />
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <h2>{pageTitle}</h2>
+      <button onClick={toggleCarsHandler}>Toggle</button>
+      { cars }
     </div>
   );
 }
