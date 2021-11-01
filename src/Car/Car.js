682673154315ec0ac829dacfb064c446cd6d1195
Listopad02 <123ayetvbass@gmail.com>
2021-11-01 18:38:03 +0300

    Radium features + some inline-styles

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,19 +1,30 @@
 import './Car.css'
+import Radium from 'radium'
 
-export default props => {
+const Car = props => {
     const inputClasses = ['input'];
+
     if (props.name !== '') {
         inputClasses.push('green');
     } else {
         inputClasses.push('red');
     }
-
     if (props.name.length > 4) {
         inputClasses.push('bold');
     }
 
+    const style = {
+        border: '1px solid #ccc', 
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        ':hover': {
+            border: '1px solid #aaa', 
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+    }
+
     return (
-        <div className="Car">
+        <div className="Car" style={style}>
             <h3>Сar name: {props.name}</h3>
             <p>Year: <strong>{props.year}</strong></p>
             <input type="text" 
@@ -25,3 +36,4 @@ export default props => {
         </div>
     )
 }
+export default Radium(Car);
